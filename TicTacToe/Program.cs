d6e7b274d90f616243225e45d8ae26c73374b194
brentwever <54568789+brentwever@users.noreply.github.com>
2019-09-11 20:31:03 +0200

    ItWorks

---
+++ TicTacToe/Program.cs
@@ -0,0 +1,130 @@
+﻿using System;
+
+namespace Brasta
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            string[,] grid = new string[,]
+            {
+                { "1", "2", "3" },
+                { "4", "5", "6" },
+                { "7", "8", "9" }
+            };
+
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("Player 1 : Wat is uw naam? : ");
+            string naam1 = Console.ReadLine();
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine("Player 2 : Wat is uw naam? : ");
+            string naam2 = Console.ReadLine();
+
+            int counter = 0;
+            bool nietdrieoprij = true;
+
+            Console.ForegroundColor = ConsoleColor.White;
+            PrintBord bordprinten = new PrintBord(grid);
+            bordprinten.BordPrintenOpConsole();
+
+            while (nietdrieoprij)
+
+            {
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine("Aantal beurten: " + counter);
+
+                // voor player 1
+                Console.WriteLine(naam1 + " : kies uw veld: ");
+                string invoerp1 = Console.ReadLine();
+                counter++;
+                Console.ForegroundColor = ConsoleColor.White;
+                bordprinten.WijzigingGridPlayer1(invoerp1);
+                bordprinten.BordPrintenOpConsole();
+
+                if ((grid[0, 0] == grid[0, 1]) && (grid[0, 1] == grid[0, 2]) && (grid[0, 0] == grid[0, 2]) ||
+                 (grid[1, 0] == grid[1, 1]) && (grid[1, 1] == grid[1, 2]) && (grid[1, 0] == grid[1, 2]) ||
+                  (grid[2, 0] == grid[2, 1]) && (grid[2, 1] == grid[2, 2]) && (grid[2, 0] == grid[2, 2]) ||
+                  (grid[0, 0] == grid[1, 0]) && (grid[1, 0] == grid[2, 0]) && (grid[2, 0] == grid[0, 0]) ||
+                  (grid[0, 1] == grid[1, 1]) && (grid[1, 1] == grid[2, 1]) && (grid[2, 1] == grid[0, 1]) ||
+                  (grid[0, 2] == grid[1, 2]) && (grid[1, 2] == grid[2, 2]) && (grid[2, 2] == grid[0, 2]) ||
+                  (grid[0, 0] == grid[1, 1]) && (grid[1, 1] == grid[2, 2]) && (grid[2, 2] == grid[0, 0]) ||
+                  (grid[2, 0] == grid[1, 1]) && (grid[1, 1] == grid[0, 2]) && (grid[0, 2] == grid[2, 0]))
+                {
+                    Console.ForegroundColor = ConsoleColor.Cyan;
+                    Console.WriteLine(naam1 + " wint!");
+                    nietdrieoprij = false;
+
+                }
+                else if (counter == 9)
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine("Helaas niemand heeft gewonnen");
+                }
+                else
+                {
+                    Console.ForegroundColor = ConsoleColor.Yellow;
+                    Console.WriteLine("Aantal beurten: " + counter);
+
+                    // voor player 2
+                    Console.WriteLine(naam2 + " : kies uw veld: ");
+                    string invoerp2 = Console.ReadLine();
+                    counter++;
+
+                    bordprinten.WijzigingGridPlayer2(invoerp2);
+                    Console.ForegroundColor = ConsoleColor.White;
+                    bordprinten.BordPrintenOpConsole();
+                    if ((grid[0, 0] == grid[0, 1]) && (grid[0, 1] == grid[0, 2]) && (grid[0, 0] == grid[0, 2]) ||
+                         (grid[1, 0] == grid[1, 1]) && (grid[1, 1] == grid[1, 2]) && (grid[1, 0] == grid[1, 2]) ||
+                         (grid[2, 0] == grid[2, 1]) && (grid[2, 1] == grid[2, 2]) && (grid[2, 0] == grid[2, 2]) ||
+                         (grid[0, 0] == grid[1, 0]) && (grid[1, 0] == grid[2, 0]) && (grid[2, 0] == grid[0, 0]) ||
+                          (grid[0, 1] == grid[1, 1]) && (grid[1, 1] == grid[2, 1]) && (grid[2, 1] == grid[0, 1]) ||
+                         (grid[0, 2] == grid[1, 2]) && (grid[1, 2] == grid[2, 2]) && (grid[2, 2] == grid[0, 2]) ||
+                          (grid[0, 0] == grid[1, 1]) && (grid[1, 1] == grid[2, 2]) && (grid[2, 2] == grid[0, 0]) ||
+                         (grid[2, 0] == grid[1, 1]) && (grid[1, 1] == grid[0, 2]) && (grid[0, 2] == grid[2, 0]))
+                    {
+                        Console.ForegroundColor = ConsoleColor.Cyan;
+                        Console.WriteLine(naam2 + " wint!");
+                        nietdrieoprij = false;
+                    }
+                    else if (counter == 9)
+                    {
+                        Console.ForegroundColor = ConsoleColor.Red;
+                        Console.WriteLine("Helaas niemand heeft gewonnen");
+                    }                    
+                    else
+                    {
+                        continue;
+                    }
+                    // einde van while loop
+                }
+
+                Console.WriteLine("GAME OVER!!");
+                Console.WriteLine("Wilt u nog een potje spelen? Y/N : ");
+                string antwoord = Console.ReadLine();
+                if (antwoord == "Y" || antwoord == "y" || antwoord == "j")
+                {
+                    counter = 0;
+                    nietdrieoprij = true;
+                    Console.Clear();
+                    bordprinten = new PrintBord(grid);
+                    bordprinten.Reset();
+                    Console.ForegroundColor = ConsoleColor.White;
+                    bordprinten.BordPrintenOpConsole();                    
+                    continue;
+                                                       }
+                else
+                {
+                    break;
+                }
+            }
+        }
+    }
+}
+
+
+
+
+       
+    
+          
+
---
+++ TicTacToe/print.cs
@@ -0,0 +1,135 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Brasta
+{
+    class PrintBord
+    {
+         //member variable
+        public string [,] grid = new string[,]
+           {
+                { "1", "2", "3" },
+                { "4", "5", "6" },
+                { "7", "8", "9" }
+           };
+
+      
+
+        //constructor
+        public PrintBord (string[,] grid)
+        {
+            this.grid = grid;
+
+        }
+
+        public void Reset()
+        {
+            this.grid[0, 0] = "1";
+            this.grid[0, 1] = "2";
+            this.grid[0, 2] = "3";
+            this.grid[1, 0] = "4";
+            this.grid[1, 1] = "5";
+            this.grid[1, 2] = "6";
+            this.grid[2, 0] = "7";
+            this.grid[2, 1] = "8";
+            this.grid[2, 2] = "9";
+        }
+        
+        public void WijzigingGridPlayer1(string invoer)
+        {
+            if (invoer == this.grid[0, 0])
+            {
+                this.grid[0, 0] = "X";
+            } else if (invoer == this.grid[0, 1])
+            {
+                this.grid[0, 1] = "X";
+            }
+            else if (invoer == this.grid[0, 2])
+            {
+                this.grid[0, 2] = "X";
+            }
+            else if (invoer == this.grid[1, 0])
+            {
+                this.grid[1, 0] = "X";
+            }
+            else if (invoer == this.grid[1, 1])
+            {
+                this.grid[1, 1] = "X";
+            }
+            else if (invoer == this.grid[1, 2])
+            {
+                this.grid[1, 2] = "X";
+            }
+            else if (invoer == this.grid[2, 0])
+            {
+                this.grid[2, 0] = "X";
+            }
+            else if (invoer == this.grid[2, 1])
+            {
+                this.grid[2, 1] = "X";
+            }
+            else if (invoer == this.grid[2, 2])
+            {
+                this.grid[2, 2] = "X";
+            }
+
+        }
+
+        public void WijzigingGridPlayer2(string invoer)
+        {
+            if (invoer == this.grid[0, 0])
+            {
+                this.grid[0, 0] = "O";
+            }
+            else if (invoer == this.grid[0, 1])
+            {
+                this.grid[0, 1] = "O";
+            }
+            else if (invoer == this.grid[0, 2])
+            {
+                this.grid[0, 2] = "O";
+            }
+            else if (invoer == this.grid[1, 0])
+            {
+                this.grid[1, 0] = "O";
+            }
+            else if (invoer == this.grid[1, 1])
+            {
+                this.grid[1, 1] = "O";
+            }
+            else if (invoer == this.grid[1, 2])
+            {
+                this.grid[1, 2] = "O";
+            }
+            else if (invoer == this.grid[2, 0])
+            {
+                this.grid[2, 0] = "O";
+            }
+            else if (invoer == this.grid[2, 1])
+            {
+                this.grid[2, 1] = "O";
+            }
+            else if (invoer == this.grid[2, 2])
+            {
+                this.grid[2, 2] = "O";
+            }
+
+        }
+
+        public void BordPrintenOpConsole()
+        {
+            
+            Console.Clear();
+            Console.WriteLine("                           ");
+            Console.WriteLine(" {0} | {1} | {2}", grid[0, 0], grid[0, 1], grid[0, 2]);
+            Console.WriteLine("----------");
+            Console.WriteLine(" {0} | {1} | {2}", grid[1, 0], grid[1, 1], grid[1, 2]);
+            Console.WriteLine("----------");
+            Console.WriteLine(" {0} | {1} | {2}", grid[2, 0], grid[2, 1], grid[2, 2]);
+            Console.WriteLine("                           ");
+
+        }
+    }
+
+}
